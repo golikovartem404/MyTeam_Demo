cf3968cadf66ebec746d81236365bdba9bcd7db3
golikovartyom <golikovartem404@gmail.com>
2022-11-21 19:26:00 +0400

    Add Network Monitor

---
+++ MyTeam/Sources/DataModel/API/NetworkMonitor.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkMonitor.swift
+//  MyTeam
+//
+//  Created by User on 21.11.2022.
+//
+
+import Foundation
+import Network
+
+final class NetworkMonitor {
+
+    static let shared = NetworkMonitor()
+    private let monitor: NWPathMonitor
+
+    private let queue = DispatchQueue(label: "NetworkConnectivityMonitor")
+    private(set) var isConnected: Bool = true
+
+    private(set) var interfaceType: NWInterface.InterfaceType?
+
+    private init() {
+        self.monitor = NWPathMonitor()
+    }
+
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.isConnected = path.status != .unsatisfied
+            self?.interfaceType = NWInterface.InterfaceType.allCases.filter { path.usesInterfaceType($0) }.first
+        }
+        monitor.start(queue: queue)
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
+
+extension NWInterface.InterfaceType: CaseIterable {
+    public static var allCases: [NWInterface.InterfaceType] = [
+        .wiredEthernet,
+        .wifi,
+        .cellular,
+        .loopback,
+        .other
+    ]
+}
