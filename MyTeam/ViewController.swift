d84a7fca2c6baf6846ae9ead557e4e9e1ad0607f
golikovartyom <golikovartem404@gmail.com>
2022-11-21 18:35:14 +0400

    Initial Commit

---
+++ MyTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTeam
+//
+//  Created by User on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
