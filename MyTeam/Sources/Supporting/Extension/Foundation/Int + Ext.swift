0d0f7ff7df85f25ef5f1045e6f9d45463733d83d
golikovartyom <golikovartem404@gmail.com>
2022-11-22 20:49:19 +0400

    Add main model and Int extension

---
+++ MyTeam/Sources/Supporting/Extension/Foundation/Int + Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Int + Ext.swift
+//  MyTeam
+//
+//  Created by User on 22.11.2022.
+//
+
+import Foundation
+
+extension Int {
+
+    static let one = 1
+    static let two = 2
+    static let three = 3
+    static let ten = 10
+}
