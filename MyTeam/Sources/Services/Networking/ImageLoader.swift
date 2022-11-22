ba9f412bbd1aecb2680302d030ae6a73679569f3
golikovartyom <golikovartem404@gmail.com>
2022-11-22 20:44:39 +0400

    Setup image loader

--- MyTeam/Sources/Services/Networking/ImageLoader.swift
+++ MyTeam/Sources/Services/Networking/ImageLoader.swift
@@ -5,4 +5,77 @@
 //  Created by User on 22.11.2022.
 //
 
-import Foundation
+import UIKit
+
+struct ImageLoader {
+
+    // MARK: - Load Image
+
+    func loadImage(from url: URL, _ onLoadWasCompleted: @escaping (_ result: Result<UIImage, Error>) -> Void) {
+        if let imageFromCache = getCacheImage(url: url) {
+            onLoadWasCompleted(.success(imageFromCache))
+            return
+        }
+
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+
+            if let error = error {
+                onLoadWasCompleted(.failure(error))
+            }
+
+            if let data = data, let response = response, let image = UIImage(data: data) {
+                saveDataToCach(with: data, response: response)
+                onLoadWasCompleted(.success(image))
+            }
+        }
+
+        dataTask.resume()
+    }
+}
+
+// MARK: - Cache
+
+private extension ImageLoader {
+
+    func getCacheImage(url: URL) -> UIImage? {
+        let urlRequest = URLRequest(url: url)
+        guard let chacheedResponse = URLCache.shared.cachedResponse(for: urlRequest) else { return nil }
+
+        return UIImage(data: chacheedResponse.data)
+    }
+
+    func saveDataToCach(with data: Data, response: URLResponse) {
+        guard let urlResponse = response.url else { return }
+        let chacheedResponse = CachedURLResponse(response: response, data: data)
+        let urlRequest = URLRequest(url: urlResponse)
+        URLCache.shared.storeCachedResponse(chacheedResponse, for: urlRequest)
+    }
+}
+
+// MARK: - UIImageView
+
+extension UIImageView {
+
+    func loadImage(from url: URL) {
+        image = nil
+
+        ImageLoader().loadImage(from: url) { [weak self] result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async { self?.image = image }
+            case .failure(let error):
+                print(error)
+                DispatchQueue.main.async { self?.image = nil }
+            }
+        }
+    }
+
+    func loadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else {
+            image = nil
+            return
+        }
+
+        loadImage(from: url)
+    }
+}
