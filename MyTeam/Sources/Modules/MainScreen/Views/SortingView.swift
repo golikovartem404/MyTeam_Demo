f2e403c30f16dc0e143a9e74f52a34a9c24758fc
golikovartyom <golikovartem404@gmail.com>
2022-11-22 19:42:32 +0400

    Add header section view and correct constraints

--- MyTeam/Sources/Modules/MainScreen/Views/SortingView.swift
+++ MyTeam/Sources/Modules/MainScreen/Views/SortingView.swift
@@ -11,21 +11,23 @@ class SortView: BaseView {
 
     let alphabetSortButton: UIButton = {
         let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "UnChecked"), for: .normal)
-        button.setBackgroundImage(UIImage(named: "isChecked"), for: .selected)
-        button.titleLabel?.text = "By alphabet"
-        button.titleLabel?.textColor = .black
-        button.titleLabel?.font = UIFont(name: "Inter-Medium", size: 16)
+        button.setTitle("By alphabet", for: .normal)
+        button.setImage(UIImage(named: "unChecked"), for: .normal)
+        button.setImage(UIImage(named: "isChecked"), for: .selected)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.titleEdgeInsets.left = 14
         return button
     }()
 
     let birthdaySortButton: UIButton = {
         let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "UnChecked"), for: .normal)
-        button.setBackgroundImage(UIImage(named: "isChecked"), for: .selected)
-        button.titleLabel?.text = "By birthday"
-        button.titleLabel?.textColor = .black
-        button.titleLabel?.font = UIFont(name: "Inter-Medium", size: 16)
+        button.setTitle("By birthday", for: .normal)
+        button.setImage(UIImage(named: "unChecked"), for: .normal)
+        button.setImage(UIImage(named: "isChecked"), for: .selected)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.titleEdgeInsets.left = 14
         return button
     }()
 
@@ -33,7 +35,7 @@ class SortView: BaseView {
         let label = UILabel()
         label.text = "Sorting"
         label.textColor = UIColor(red: 0.02, green: 0.02, blue: 0.063, alpha: 1)
-        label.font = UIFont(name: "Inter-SemiBlond", size: 20)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
         return label
     }()
 
@@ -56,13 +58,15 @@ class SortView: BaseView {
         }
 
         alphabetSortButton.snp.makeConstraints { make in
-            make.top.equalTo(self.snp.top).offset(84)
-            make.leading.equalTo(self.snp.leading).offset(18)
+            make.top.equalTo(titleLabel.snp.bottom).offset(41.5)
+            make.leading.equalTo(self.snp.leading).offset(26)
+            make.trailing.equalTo(self.snp.trailing).offset(-214)
         }
 
         birthdaySortButton.snp.makeConstraints { make in
-            make.top.equalTo(alphabetSortButton.snp.bottom).offset(40)
+            make.top.equalTo(alphabetSortButton.snp.bottom).offset(35)
             make.leading.equalTo(alphabetSortButton.snp.leading)
+            make.trailing.equalTo(self.snp.trailing).offset(-172)
         }
     }
 }
